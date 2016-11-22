cb5eff34924de27f345ccd76918c5299d9856e55
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2016-11-22 05:29:58 -0500

    create a simple and empty android app

---
+++ test/create_simple.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export JAVA_HOME=/home/peyrardj/local/jdk1.8.0_111/
+export ANDROID_HOME=/home/peyrardj/local/android-sdk-linux/
+export NDK_STANDALONE=/home/peyrardj/local/android-ndk-r13b/
+export PATH=$JAVA_HOME/bin/:$PATH:$NDK_STANDALONE:$ANDROID_HOME/tools
+
+android create project --name AndroidHelloJNI --activity JNIActivity --package "com.mytest"   --path . --target 1
+ant
+ant debug
+find .|grep apk
+
