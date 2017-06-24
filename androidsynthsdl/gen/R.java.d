793cd2b9805f30d26277b2beb684eaadc856212c
johann peyrard <yoyz2k@yahoo.fr>
2017-06-24 14:41:24 +0200

    first try on sdl synth on android

---
+++ androidsynthsdl/gen/R.java.d
@@ -0,0 +1,12 @@
+/home/yoyz/build/android/android/androidsdl/gen/org/libsdl/testgles/R.java \
+ : /home/yoyz/build/android/android/androidsdl/res/drawable-hdpi/ic_launcher.png \
+/home/yoyz/build/android/android/androidsdl/res/values/strings.xml \
+/home/yoyz/build/android/android/androidsdl/res/layout/main.xml \
+/home/yoyz/build/android/android/androidsdl/res/drawable-mdpi/ic_launcher.png \
+/home/yoyz/build/android/android/androidsdl/res/drawable-xxhdpi/ic_launcher.png \
+/home/yoyz/build/android/android/androidsdl/res/drawable-xhdpi/ic_launcher.png \
+/home/yoyz/build/android/android/androidsdl/bin/res/drawable-hdpi/ic_launcher.png \
+/home/yoyz/build/android/android/androidsdl/bin/res/drawable-mdpi/ic_launcher.png \
+/home/yoyz/build/android/android/androidsdl/bin/res/drawable-xxhdpi/ic_launcher.png \
+/home/yoyz/build/android/android/androidsdl/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/yoyz/build/android/android/androidsdl/bin/AndroidManifest.xml \
