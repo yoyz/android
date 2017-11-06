8f3b1cec3129e89096bb10a83d24088de9cffa36
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-11-06 23:45:34 +0100

    file_to_remove

--- androidsynthsdl/gen/R.java.d
+++
@@ -1,8 +0,0 @@
-/home/peyrardj/build/android/android/androidsynthsdl/gen/org/libsdl/testgles/R.java \
- : /home/peyrardj/build/android/android/androidsynthsdl/res/drawable-mdpi/ic_launcher.png \
-/home/peyrardj/build/android/android/androidsynthsdl/res/drawable-hdpi/ic_launcher.png \
-/home/peyrardj/build/android/android/androidsynthsdl/res/values/strings.xml \
-/home/peyrardj/build/android/android/androidsynthsdl/res/layout/main.xml \
-/home/peyrardj/build/android/android/androidsynthsdl/res/drawable-xhdpi/ic_launcher.png \
-/home/peyrardj/build/android/android/androidsynthsdl/res/drawable-xxhdpi/ic_launcher.png \
-/home/peyrardj/build/android/android/androidsynthsdl/bin/AndroidManifest.xml \
